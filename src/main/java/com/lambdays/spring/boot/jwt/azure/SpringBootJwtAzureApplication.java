00983f24b6926a396c57f3324601ad24f5ed21a9
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-07-04 19:54:23 -0300

    inicializacao de repositorio

---
+++ src/main/java/com/lambdays/spring/boot/jwt/azure/SpringBootJwtAzureApplication.java
@@ -0,0 +1,13 @@
+package com.lambdays.spring.boot.jwt.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambdays/spring/boot/jwt/azure/SpringBootJwtAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdays.spring.boot.jwt.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
